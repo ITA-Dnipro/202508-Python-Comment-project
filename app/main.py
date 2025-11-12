aab1d9470593be24252f61cedcababb3c52a2e77
ilia <fleshgolub@gmail.com>
2025-11-12 12:46:20 +0200

    fast fix

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI, Request
 from app.routers import comments
-import logging
-
-logger = logging.getLogger("headers_logger")
-logging.basicConfig(level=logging.INFO)
 
 app = FastAPI(title="Comments Microservice")
 
@@ -12,4 +8,3 @@ app.include_router(comments.router, prefix="/api", tags=["comments"])
 @app.get("/")
 async def root():
     return {"message": "Comments service is running"}
-
