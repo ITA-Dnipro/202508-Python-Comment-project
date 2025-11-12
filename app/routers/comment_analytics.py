96e956c381f0618d53c62b7b3cf12fa6267bb96a
ilia <fleshgolub@gmail.com>
2025-11-12 17:13:53 +0200

    implement new func

--- app/routers/comment_analytics.py
+++ app/routers/comment_analytics.py
@@ -77,6 +77,8 @@ async def get_global_comment_analytics(http_user_id: int = Header(None, alias="u
 
     # Fetch all startup's projects
     async with httpx.AsyncClient() as client:
+        # ToDo: smplement this endpoint in monolith(get startup projects from current startuper)
+        # now endpoint returns all projects
         resp = await client.get(f"{PROJECTS_SERVICE_URL}/?owner={http_user_id}", headers={"Authorization": token})
         if resp.status_code != 200:
             logger.error(f"Failed to fetch projects for user {http_user_id}: {resp.text}")
